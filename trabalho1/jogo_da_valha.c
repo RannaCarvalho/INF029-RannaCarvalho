dff08fe9f3f5b599926c6bec0e0bec5e3e73c867
RannaCarvalho <rannadecarvalho@gmail.com>
2025-11-18 15:27:26 -0300

    Update jogo_da_valha.c

--- trabalho1/jogo_da_valha.c
+++ trabalho1/jogo_da_valha.c
@@ -1,22 +1,17 @@
 #include <stdio.h>
+#include <ctype.h>
 #define N 3
 
 void mostrar_tabuleiro(char tab[N][N]) {
-    int i, j;
-    printf("\n   1   2   3\n");
-    for (i = 0; i < N; i++) {
-        printf("%d ", i + 1);
-        for (j = 0; j < N; j++) {
-            printf(" %c ", tab[i][j]);
-            if (j < N - 1) printf("|");
-        }
-        printf("\n");
-        if (i < N - 1) printf("  ---+---+---\n");
-    }
-    printf("\n");
+    printf("\n    1   2   3\n");
+    printf("A   %c | %c | %c\n", tab[0][0], tab[0][1], tab[0][2]);
+    printf("   ---+---+---\n");
+    printf("B   %c | %c | %c\n", tab[1][0], tab[1][1], tab[1][2]);
+    printf("   ---+---+---\n");
+    printf("C   %c | %c | %c\n\n", tab[2][0], tab[2][1], tab[2][2]);
 }
 
-int checar_vitoria(char tab[N][N], char *vencedor) {
+int verificar_vencedor(char tab[N][N], char *vencedor) {
     int i;
 
     for (i = 0; i < N; i++) {
@@ -37,6 +32,7 @@ int checar_vitoria(char tab[N][N], char *vencedor) {
         *vencedor = tab[0][0];
         return 1;
     }
+
     if (tab[0][2] != ' ' && tab[0][2] == tab[1][1] && tab[1][1] == tab[2][0]) {
         *vencedor = tab[0][2];
         return 1;
@@ -47,20 +43,34 @@ int checar_vitoria(char tab[N][N], char *vencedor) {
 
 int tabuleiro_cheio(char tab[N][N]) {
     int i, j;
+
     for (i = 0; i < N; i++)
         for (j = 0; j < N; j++)
             if (tab[i][j] == ' ')
                 return 0;
+
+    return 1;
+}
+
+int converter_celula(char entrada[], int *linha, int *coluna) {
+    char letra = toupper(entrada[0]);
+    char numero = entrada[1];
+
+    if (letra < 'A' || letra > 'C') return 0;
+    if (numero < '1' || numero > '3') return 0;
+
+    *linha = letra - 'A';
+    *coluna = numero - '1';
+
     return 1;
 }
 
 int main() {
     char tabuleiro[N][N];
     int linha, coluna;
-    int jogador = 1; 
-    char marca;
-    char vencedor = ' ';
-    int jogada_valida;
+    char entrada[3];
+    int jogador = 1;
+    char marca, vencedor = ' ';
 
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
@@ -69,47 +79,39 @@ int main() {
     while (1) {
         mostrar_tabuleiro(tabuleiro);
 
-        if (jogador == 1) marca = 'X';
-        else marca = '0'; 
+        marca = (jogador == 1 ? 'X' : '0');
 
-        printf("Jogador %d, informe sua jogada:\n", jogador);
-        printf("Informe linha (1-3) e coluna (1-3), separados por espaço: ");
-        jogada_valida = scanf("%d %d", &linha, &coluna);
+        printf("Jogador %d (%c), informe a celula (ex: B3): ", jogador, marca);
+        scanf("%2s", entrada);
 
-        while (getchar() != '\n');
-
-        if (jogada_valida != 2) {
-            printf("Entrada invalida. Tente novamente.\n\n");
+        if (!converter_celula(entrada, &linha, &coluna)) {
+            printf("Entrada invalida! Use A1, B3, C2, etc.\n\n");
             continue;
         }
 
-        linha--; coluna--;
-
-        if (linha < 0 || linha >= N || coluna < 0 || coluna >= N) {
-            printf("Posicao invalida. Linhas e colunas devem estar entre 1 e 3.\n\n");
-            continue;
-        }
         if (tabuleiro[linha][coluna] != ' ') {
-            printf("Posicao ja ocupada. Escolha outra.\n\n");
+            printf("Celula ocupada! Escolha outra.\n\n");
             continue;
         }
 
         tabuleiro[linha][coluna] = marca;
 
-        if (checar_vitoria(tabuleiro, &vencedor)) {
+        if (verificar_vencedor(tabuleiro, &vencedor)) {
             mostrar_tabuleiro(tabuleiro);
-            if (vencedor == 'X') printf("Jogador 1 (X) venceu! Parabens!\n");
-            else printf("Jogador 2 (0) venceu! Parabens!\n");
+            if (vencedor == 'X') printf("Jogador 1 venceu!\n");
+            else printf("Jogador 2 venceu!\n");
             break;
         }
 
-        // Checa empate
         if (tabuleiro_cheio(tabuleiro)) {
             mostrar_tabuleiro(tabuleiro);
-            printf("Empate! Tabuleiro cheio.\n");
+            printf("Nao houve vencedor. Deu empate!\n");
             break;
         }
-
+        jogador = (jogador == 1 ? 2 : 1);
+    }
+    return 0;
+}
         jogador = (jogador == 1) ? 2 : 1;
     }
 
